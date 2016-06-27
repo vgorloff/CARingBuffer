a4804f67a331850e3ffff7eb5ca74a8269944ff8
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2016-06-27 22:03:59 +0200

    Project: Added target for testing audio.

---
+++ CAPlayThrough/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CAPlayThrough
+//
+//  Created by Vlad Gorlov on 27.06.16.
+//  Copyright © 2016 WaveLabs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
