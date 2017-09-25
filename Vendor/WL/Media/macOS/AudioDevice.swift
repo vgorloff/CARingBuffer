380e71549408750b8683dae84ddec6aa9f82cbd9
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2017-09-25 22:57:46 +0200

    Infrastructure update.

--- Vendor/WL/Media/macOS/AudioDevice.swift
+++ Vendor/WL/Media/macOS/AudioDevice.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2016 WaveLabs. All rights reserved.
 //
 
-#if os(OSX)
 import CoreAudio
 
 public final class AudioDevice {
@@ -160,4 +159,3 @@ public final class AudioDevice {
       }
    }
 }
-#endif
