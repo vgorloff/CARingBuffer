6175a954f4fe091203c20b8f15694e9cd6ed2442
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2019-02-09 11:31:42 +0100

    Structure update.

---
+++ Sources/CAPlayThrough/Logger.swift
@@ -0,0 +1,15 @@
+//
+//  Logger.swift
+//  CAPlayThrough-macOS
+//
+//  Created by Vlad Gorlov on 09.02.19.
+//  Copyright © 2019 WaveLabs. All rights reserved.
+//
+
+import Foundation
+
+enum ModuleLogCategory: String, LogCategory {
+   case media, core, controller
+}
+
+let log = Log<ModuleLogCategory>(subsystem: "ringBuffer")
